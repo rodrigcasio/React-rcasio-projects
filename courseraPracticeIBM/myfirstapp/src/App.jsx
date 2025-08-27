124a1ec152c54994412b5edd8603e9791e88859a
rodrigcasio <rod.casio@outlook.com>
2025-08-27 17:54:07 -0600

    Modified the conten of App.jsx file

--- courseraPracticeIBM/myfirstapp/src/App.jsx
+++ courseraPracticeIBM/myfirstapp/src/App.jsx
@@ -3,6 +3,8 @@ function App() {
     <div>
       <h1>Hello World!</h1>
       <p>This is my first time using JSX and it is awesome!</p>
+      <p>Today is <strong>August 27, 2025</strong> and my name is Rodrigo Casio</p>
+      <p>I will become a great developer and engineer!</p>
     </div>
   );
 }
