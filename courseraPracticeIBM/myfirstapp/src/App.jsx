4c3f4e1219da4a79dedae69c55c0fbbe3047f54f
rodrigcasio <rod.casio@outlook.com>
2025-08-27 19:49:39 -0600

    Added Created file ListFavLanguages.jsx

--- courseraPracticeIBM/myfirstapp/src/App.jsx
+++ courseraPracticeIBM/myfirstapp/src/App.jsx
@@ -1,12 +1,37 @@
-function App() {
+function App(props) { // primary component , it is a defualt export
+  const currDate = new Date();
+
   return (
     <div>
       <h1>Hello World!</h1>
+      <h2>The time now is {currDate.toLocaleTimeString()}.</h2>
       <p>This is my first time using JSX and it is awesome!</p>
       <p>Today is <strong>August 27, 2025</strong> and my name is Rodrigo Casio</p>
       <p>I will become a great developer and engineer!</p>
+      {/*child div here adding components inside this compontens*/}
+      <div>
+        <Comp2A />
+        <Comp3A />
+      </div>
     </div>
   );
 }
+
+// secondary component, they are named exports  
+// A : App
+export function Comp2A(){
+  return (
+    <div>
+      <h2>Im component2 im being called from the App component, which im inside of it</h2>
+    </div>
+  );
+}
+
+export function Comp3A(){
+  return (
+    <p>Hello, im component 3, and im being called from component App <strong>(im inside of it too!)</strong></p>
+  );
+}
+
 
 export default App;
--- courseraPracticeIBM/myfirstapp/src/main.jsx
+++ courseraPracticeIBM/myfirstapp/src/main.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'
+import App, { Comp2A, Comp3A } from './App.jsx'
+import ListFavLanguages, {Comp1LFL, Comp2LFL} from './ListFavLanguages.jsx' 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// inserting components inside the root render
 root.render(
+  <>
   <App />
-)
+  <ListFavLanguages />
+  </>
+);
