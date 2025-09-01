47c08ec323e5c9e3895280f43c65b55e505d8805
rodrigcasio <rod.casio@outlook.com>
2025-09-01 11:26:43 -0600

    Added comments and explanation for the use of the (prevState) parameter used in the both functions

--- courseraPracticeIBM/content_rating_app/src/Components/ContentRating.jsx
+++ courseraPracticeIBM/content_rating_app/src/Components/ContentRating.jsx
@@ -7,11 +7,11 @@ class ContentRating extends Component {
     constructor(){
         super();
         this.state = { likes: 0, dislikes: 0, totalRatings: 0 };     // this.state is a property | { likes: 0, dislikes: 0 } object with a collection of key-value pair.
-    }
+    }                                                                // totalRatings key-value pair added for extra-practice to show the sum of likes and dislikes
     // methods
     handleLikes = () => {           // returning an object implicitly, that is why we use this ({ likes: preState.likes + 1 }) inner parenthesis.
-        this.setState((prevState) => ({ likes: prevState.likes + 1, totalRatings: prevState.totalRatings + 1 }));      // added for practice totalRatings 
-    }
+        this.setState((prevState) => ({ likes: prevState.likes + 1, totalRatings: prevState.totalRatings + 1 }));      // added for practice totalRatings: prevState.totalRatings + 1
+    }                                                                                                                  
     handleDislikes = () => {
         this.setState((prevState) => ({ dislikes: prevState.dislikes + 1, totalRatings: prevState.totalRatings + 1 }));
     }
@@ -34,3 +34,21 @@ class ContentRating extends Component {
     }
 }
 export default ContentRating;
+
+/*
+        handleDislikes = () => {
+        this.setState((prevState) => ({ dislikes: prevState.dislikes + 1, totalRatings: prevState.totalRatings + 1 }));
+    }
+        Explanation of using the parameter (prevState)
+
+        prevState parameter is a snapshot of a component's state "just before" the setState operation takes place.
+        It's a way for React to garantee that your update is based on the most accurate state value, preventing bugs that can arise from asynchronous updates
+       a "snapshot of the component's state" is a copy of the this.state object at the exact moment before React is about to process your update. 
+
+       in short terms:
+
+        A snapshot of the state before an update.
+        Use prevState to avoid stale data because setState is asynchronous.
+        It guarantees that you're working with the most recent values for a consistent and correct count.
+
+*/
