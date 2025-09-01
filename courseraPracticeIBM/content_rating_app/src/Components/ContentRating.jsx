f6dba53c4ee7a7f50b9e6934e26b6c7da8aa2ac5
rodrigcasio <rod.casio@outlook.com>
2025-09-01 09:39:50 -0600

    Added class component template for "Content Rating" component

---
+++ courseraPracticeIBM/content_rating_app/src/Components/ContentRating.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './ContentRating.css'
+
+class ContentRating extends Component {
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <>
+            <h1>Text Content Rating</h1>
+            </>
+        );
+    }
+}
+export default ContentRating;
