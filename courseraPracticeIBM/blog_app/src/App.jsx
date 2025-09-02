ace7a28e460cf0a32e1461011d2009803ce265a9
rodrigcasio <rod.casio@outlook.com>
2025-09-02 17:50:53 -0600

    Added a better description for the line of the variable novel

--- courseraPracticeIBM/blog_app/src/App.jsx
+++ courseraPracticeIBM/blog_app/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NovelBlog from './Components/NovelBlog';
 
 function App() {
-  const novel = {   // created an instantiation of the novel component with appropiate data for title, author, description, and type components 
+  const novel = {   // this is justa local variable variable that holds appropiate data for title, author, description, and type components 
     title: 'Harry Potter',
     author: 'J. K. Rowling',
     description: 'A world full of wizards and magic',
