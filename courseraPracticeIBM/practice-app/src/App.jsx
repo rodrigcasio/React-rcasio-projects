c7e2d8ac4633c548a1dd25d98b9351f35ba903ea
rodrigcasio <rod.casio@outlook.com>
2025-09-02 11:09:07 -0600

    Added "SecondChildApp" component to the parent App component and sending data from App to SecondChildAppEx

--- courseraPracticeIBM/practice-app/src/App.jsx
+++ courseraPracticeIBM/practice-app/src/App.jsx
@@ -1,14 +1,16 @@
 import CounterButton from './components/CounterButton.jsx'
 import FirstChildAppEx from './components/FirstChildAppEx.jsx'
 import ParentComp from './components/ParentComp.jsx'
+import SecondChildAppEx from './components/SecondChildAppEx.jsx'
 
 function App() {
-
+    const employee_id = 10007;    // example of passing prop dynamically to the SecondChildAppEx component using {}.
   return (
     <>
       <CounterButton />
       <ParentComp />
       <FirstChildAppEx  name="Rodrigo" /> 
+      <SecondChildAppEx name="Rodrigo Casio Garcia" employeeID={employee_id} dept_id="123477" />
     </>
   )
 }
