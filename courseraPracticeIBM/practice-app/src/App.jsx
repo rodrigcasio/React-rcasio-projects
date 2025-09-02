15e5a86e53e21f6730342a2397780e6a71d39f34
rodrigcasio <rod.casio@outlook.com>
2025-09-02 10:52:21 -0600

    Added "CounterButton" component to App parent component

--- courseraPracticeIBM/practice-app/src/App.jsx
+++ courseraPracticeIBM/practice-app/src/App.jsx
@@ -1,20 +1,17 @@
-import { useState } from 'react'
 import './App.css'
+import CounterButton from './components/CounterButton.jsx'
 import ParentComp from './components/ParentComp.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <ParentComp />
-      </div>
+      <CounterButton />
+      <ParentComp />
     </>
   )
 }
 
 export default App
+
+
