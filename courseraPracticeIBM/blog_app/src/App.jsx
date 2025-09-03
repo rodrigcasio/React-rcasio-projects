ba03d922913b7a1ad738f69c06ff2485e7365da6
rodrigcasio <rod.casio@outlook.com>
2025-09-02 18:20:49 -0600

    Corrected string value of the body key

--- courseraPracticeIBM/blog_app/src/App.jsx
+++ courseraPracticeIBM/blog_app/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     author: 'J. K. Rowling',
     description: 'A world full of wizards and magic',
     type: 'Fiction',
-    body: "Harry Potter is about an orphaned boy who discovers he's a wizard and attends a magical school called Hogwarts. He and his friends navigate their studies while uncovering secrets about his past and facing off against the dark wizard who killed his parents.Harry Potter is a seven-book fantasy series about a young wizard named Harry Potter and his friends Hermione Granger and Ron Weasley. The story follows their years at Hogwarts School of Witchcraft and Wizardry, where they face the growing threat of Lord Voldemort, the dark wizard who murdered Harry's parents. The series explores themes of love, friendship, loss, and the eternal battle between good and evil as Harry uncovers the truth about his past and prepares for his final confrontation with Voldemort."
+    body: "Harry Potter is about an orphaned boy who discovers he's a wizard and attends a magical school called Hogwarts. He and his friends navigate their studies while uncovering secrets about his past and facing off against the dark wizard who killed his parents. Harry Potter is a seven-book fantasy series about a young wizard named Harry Potter and his friends Hermione Granger and Ron Weasley. The story follows their years at Hogwarts School of Witchcraft and Wizardry, where they face the growing threat of Lord Voldemort, the dark wizard who murdered Harry's parents. The series explores themes of love, friendship, loss, and the eternal battle between good and evil as Harry uncovers the truth about his past and prepares for his final confrontation with Voldemort."
   };
 
   return (
