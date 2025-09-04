bc0c8225f1963ae0517b64c6f214889acb2611aa
rodrigcasio <rod.casio@outlook.com>
2025-09-03 21:29:22 -0600

    Modified styles from "EventPlanner.css"

--- courseraPracticeIBM/event_planner_app/src/main.jsx
+++ courseraPracticeIBM/event_planner_app/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+// import './index.css' not needed for this exercise 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
