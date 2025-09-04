055e1f9971035b7d9806550aaca2a3835d439c3a
rodrigcasio <rod.casio@outlook.com>
2025-09-03 19:43:15 -0600

    Added basic html structure for the component "EventPlanner"

--- courseraPracticeIBM/event_planner_app/src/Components/EventPlanner.jsx
+++ courseraPracticeIBM/event_planner_app/src/Components/EventPlanner.jsx
@@ -1,1 +1,29 @@
+import React from 'react';
+import './EventPlanner';
 
+const EventPlanner = () => {
+    return (
+        <div className="event-planner-container">
+            <header>
+                <h1>Welcome to Event Planner</h1>
+            </header>
+            <section className="description">
+                
+            </section>
+            <section className="event_categories">
+                
+            </section>
+            <section className="features">
+
+            </section>
+            <section className="testimonials">
+
+            </section>
+            <section className="contact">
+
+            </section>
+        </div>
+
+    );
+}
+export default EventPlanner;
