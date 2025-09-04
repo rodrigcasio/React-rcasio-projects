3299192acadf770413be1dd2ed9e506eb4586c1b
rodrigcasio <rod.casio@outlook.com>
2025-09-03 20:28:33 -0600

    Added conent for the "description and events_categories" sections

--- courseraPracticeIBM/event_planner_app/src/Components/EventPlanner.jsx
+++ courseraPracticeIBM/event_planner_app/src/Components/EventPlanner.jsx
@@ -8,10 +8,37 @@ const EventPlanner = () => {
                 <h1>Welcome to Event Planner</h1>
             </header>
             <section className="description">
-                
+                <p>Plan and organize your events effortlessly with Event Planner. From birthdays to corporate meetings, we've got you covered.</p>
+                <button className="get-started-button">Get Started</button>
             </section>
-            <section className="event_categories">
-                
+            <section className="events_categories">
+                <ul>
+                    <h2>Social Events:</h2>
+                    <li>Birthday parties</li>
+                    <li>Anniversary celebrations</li>
+                    <li>Wedding receptions</li>
+                    <li>Baby Showers</li>
+                    <li>Graduations parties</li>
+                    <li>Family Reunions</li>
+                </ul>
+                <ul>
+                    <h2>Entretainment Events:</h2>
+                    <li>Concerts</li>
+                    <li>Music Festivals</li>
+                    <li>Film Screenings</li>
+                    <li>Comedy Shows</li>
+                    <li>Art Exhibitions</li>
+                    <li>Cultural Events</li>
+                </ul>
+                <ul>
+                    <h2>Community Events:</h2>
+                    <li>Fundraising Events</li>
+                    <li>Charity Galas</li>
+                    <li>Volunteer Drives</li>
+                    <li>Neighborhood block parties</li>
+                    <li>Community Festivals</li>
+                    <li>Cultural Cultural Celebrations</li>
+                </ul>
             </section>
             <section className="features">
 
