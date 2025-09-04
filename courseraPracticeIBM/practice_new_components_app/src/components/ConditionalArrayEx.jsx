229e5288e48982e91d52929447f306f43bf9641e
rodrigcasio <rod.casio@outlook.com>
2025-09-04 11:59:52 -0600

    Modified ternary condition, missing the length and added a new <div> tag into each outcome

--- courseraPracticeIBM/practice_new_components_app/src/components/ConditionalArrayEx.jsx
+++ courseraPracticeIBM/practice_new_components_app/src/components/ConditionalArrayEx.jsx
@@ -3,19 +3,25 @@ import React from 'react';
 
 // example of a component that uses conditional rendering to display the contents of an array in a list <li> tag elements
 const ConditionalArrayEx = () => {
-    const [items, setItems] = useState(['React', 'Vue', 'Angular', 'Vanilla ']);
+    const [items] = useState(['React', 'Vue', 'Angular', 'Vanilla ']);
     
     return (
         <div>
             <h1>Front-end Languages</h1>
-            {items > 0 ? (
+            {items.length > 0 ? (
+                <div>   {/* added a new div if wanted to add a new <h4>*/}
+                <h4>Non-empty array</h4>
                 <ul>
                     {items.map((item, index) => (
-                        <li id={index}>{item}</li>
+                        <li key={index}>{item}</li>
                     ))}
                 </ul>
+                </div>
             ) : (
-                <p>No Font-end languages are avaiable</p>
+                <div>
+                <h4>Empty array</h4>
+                <p>No Front-end languages are avaiable</p>
+                </div>
             )}
         </div>
     );   
