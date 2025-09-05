b123ff1c46cfc8882ba566898397c5843f840107
rodrigcasio <rod.casio@outlook.com>
2025-09-05 17:27:29 -0600

    Added content to the "SecondChildComponent" with explanation of why these components are done

--- courseraPracticeIBM/practice_new_components_app/src/componentsDOMUpdate/FirstChildComponent.jsx
+++ courseraPracticeIBM/practice_new_components_app/src/componentsDOMUpdate/FirstChildComponent.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { useState } from 'react';
 
 // the first child component renders a list of items, and the parent (App) component updates only one item
-// The first child component has a list of
-// array items with one button to
-// update the first element of that item.
-// The second child component only
-// shows information related to that component. 
 
 const FirstChildComponent = () => {
     const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3']);
@@ -20,7 +15,7 @@ const FirstChildComponent = () => {
     console.log('Rendering First Child Component');
     return (
         <div>
-            <button onClick={updateItem}>Update Item</button>         
+            <button onClick={updateItem}>Update Item</button> {/* this allows to update the first item */}
             <ul> {items.map((item, index) => (              // creates a li tag the map() for each item
                 <li key={index}>{item}</li> ))} 
             </ul>
@@ -28,3 +23,31 @@ const FirstChildComponent = () => {
     );
 }
 export default FirstChildComponent;
+
+
+// In this example, the app component
+// maintains the state of a list of items.
+// The first child component has a list of
+// array items with one button to
+// update the first element of that item.
+// The second child component only
+// shows information related to that component. 
+
+// Explanation
+/*
+    IBM explanation: 
+    if we inspect the console logs, we'll notice the following behavior.
+
+"For the first time loading,
+all the components will render.
+When you select to update the item,
+only the first child component will be re-rendered.
+Neither the app component nor
+the second child component will change.
+React's virtual DOM compares the updated app with
+the previous virtual DOM and determines that
+only the state of the first child component has changed.
+Therefore, it only updates the relevant part of the DOM,
+the child component, without re-rendering the entire list. "  : IBM.
+
+*/
---
+++ courseraPracticeIBM/practice_new_components_app/src/componentsDOMUpdate/SecondChildCompoent.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const SecondChildComponent = React.memo(() => {
+    console.log('Rendering SecondChildComponent');
+    
+    return(
+        <div>
+            <h2>Second Child Component</h2>
+            <p>This is the second child component.</p>
+        </div>
+    );
+});
+export default SecondChildComponent;
+
+// Explanation
+/*
+    The second child componet only shows information related to that component.
+
+    IBM explanation: 
+    if we inspect the console logs, we'll notice the following behavior.
+
+"For the first time loading,
+all the components will render.
+When you select to update the item,
+only the first child component will be re-rendered.
+Neither the app component nor
+the second child component will change.
+React's virtual DOM compares the updated app with
+the previous virtual DOM and determines that
+only the state of the first child component has changed.
+Therefore, it only updates the relevant part of the DOM,
+the child component, without re-rendering the entire list. "  : IBM.
+
+*/
