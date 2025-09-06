6e55533669f45db6991ab1a2b67d5ede3e0ba1d7
rodrigcasio <rod.casio@outlook.com>
2025-09-05 19:34:10 -0600

    Added HTML structure for the return() of the TodoList.jsx component

--- courseraPracticeIBM/todo_list_app/src/Components/TodoList.jsx
+++ courseraPracticeIBM/todo_list_app/src/Components/TodoList.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import {useState} from 'react';
+import './TodoList.css';
 
+const TodoList = () => {
+    
+    return(
+        <>
+            <div>
+                <h1></h1>
+                <div>
+                    <input />
+                    <button></button>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </>
+    );
+}
+export default TodoList;
