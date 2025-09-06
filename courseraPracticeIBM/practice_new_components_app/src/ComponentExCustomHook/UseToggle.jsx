a780596d5c5a9b37babb758ae910b433d02ba828
rodrigcasio <rod.casio@outlook.com>
2025-09-06 11:29:16 -0600

    Added functionality for the component "ToggleButtonCustomHook" and setting up the function "toggle" the custom hook file "UseToggle"

--- courseraPracticeIBM/practice_new_components_app/src/ComponentExCustomHook/UseToggle.jsx
+++ courseraPracticeIBM/practice_new_components_app/src/ComponentExCustomHook/UseToggle.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import {useState} from 'react';
 
+// example of UseToggle custom hook is used to manage the state of a buttton
+
+const UseToggle = (initialValue = false) => {
+    const[value, setValue] = useState(initialValue);
+
+    const toggle = () => {
+        setValue(!value);
+    }
+
+    return [value, toggle];         // return two values, "value" = isToggled : representing the current state (either true of false)
+}                                                    // and "toggle" : a function that toggles the state between true and false when invoked)
+export default UseToggle;
