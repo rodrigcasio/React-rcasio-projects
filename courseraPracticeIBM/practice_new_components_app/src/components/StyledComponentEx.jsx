f73f98fac8e7e24788637f9fe908826d2a63423c
rodrigcasio <rod.casio@outlook.com>
2025-09-05 19:04:03 -0600

    Aded documentation for the use of CSS style property "Display" inside a JavaScript object

--- courseraPracticeIBM/practice_new_components_app/src/components/StyledComponentEx.jsx
+++ courseraPracticeIBM/practice_new_components_app/src/components/StyledComponentEx.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {useState} from 'react';
 
-// Example of a component with CSS style applied by using JavaScript objects 
-
 const StyledComponentEx = () => {
     const [isVisible, setIsVisible] = useState(true);
 
@@ -26,3 +24,10 @@ const StyledComponentEx = () => {
     );
 }
 export default StyledComponentEx;
+
+    // Explanation of using the CSS property 'display: 'block' or 'none';
+// Example of a component with CSS style applied by using JavaScript objects 
+// the property display in css, when is set to 'block' the element behaves as a block-level element
+// when block, take up a full available width and their parent container
+// Basically, it controls the layout behavior of an element on a web page, depending display: 'block' or 'none'.
+// it dictates how an element is rendered and interacts with the document flow
