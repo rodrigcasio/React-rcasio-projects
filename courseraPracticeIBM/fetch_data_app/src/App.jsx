457bb8cd58ea8f9dbfec9a093205011ba55ec5e5
rodrigcasio <rod.casio@outlook.com>
2025-09-06 13:10:26 -0600

    Imported component into the App parent component to verify if data is being shown in the console tab.

--- courseraPracticeIBM/fetch_data_app/src/App.jsx
+++ courseraPracticeIBM/fetch_data_app/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import FetchData from './Components/FetchData'
+import FetchYogaData from './Components/FetchYogaData'
 
 function App() {
 
   return (
     <>
       <FetchData />
+      {/* Exercise : */}
+      <FetchYogaData />
     </>
   )
 }
