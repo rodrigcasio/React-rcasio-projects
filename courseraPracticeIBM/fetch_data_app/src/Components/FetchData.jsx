6ef254dcd3bcb7ce2a2bbf877eff872c4f61eb79
rodrigcasio <rod.casio@outlook.com>
2025-09-06 12:58:27 -0600

    Modified the HTML structure of the Component "FetchData.jsx" and imported into this file the styles used in "FEtchData.css"

--- courseraPracticeIBM/fetch_data_app/src/Components/FetchData.jsx
+++ courseraPracticeIBM/fetch_data_app/src/Components/FetchData.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import UseFetch from './UseFetch.jsx';
+import './FetchData.css'
 
 const FetchData = () => {
     const [data] = UseFetch('https://api.npoint.io/9045c260b1565daa9e15');      // setting the url into the value of data 
@@ -7,15 +8,14 @@ const FetchData = () => {
 
     return (
         <>
+            <h1 className="usefetch_heading">Use Fetch Custom Hook</h1>
             <ul className="list_data_main">
-                <h1 className="usefetch_heading">Use Fetch Custom Hook</h1>
                 {data && data.map((e, index) => (    // conditional if data is true.. we can fetch data using 'e' parameter to access each type of data passed e.
                     <li key={index} className="list_data">
                         <h3>{e.name}</h3>
                         <p><strong>Importance: </strong>{e.importance}</p>
                         <p><stong>Benefits: </stong>{e.benefits}</p>
                         <p><strong>Best time to intake: </strong>{e.best_time_to_intake}</p>
-                        <a href={e.image}></a>
                     </li>
                 ))}
             </ul>
