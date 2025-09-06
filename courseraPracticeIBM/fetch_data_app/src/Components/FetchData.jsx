95a1c54d9ce3ec44bec111063fe6358835440841
rodrigcasio <rod.casio@outlook.com>
2025-09-06 12:33:55 -0600

    Fetching Data using the custom hook "UseFetch.jsx" in the component "FetchData.jsx"

--- courseraPracticeIBM/fetch_data_app/src/Components/FetchData.jsx
+++ courseraPracticeIBM/fetch_data_app/src/Components/FetchData.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import UseFetch from './UseFetch.jsx';
 
 const FetchData = () => {
+    const [data] = UseFetch('https://api.npoint.io/9045c260b1565daa9e15');      // setting the url into the value of data 
+    console.log(data);      // checking if the data was retrieved in the variable data, can be viewed in the console tab of the website
+
     return (
         <>
             <ul className="list_data_main">
