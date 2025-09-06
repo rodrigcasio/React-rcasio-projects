916622581b20dc37e98072411a6c40177aa01cbc
rodrigcasio <rod.casio@outlook.com>
2025-09-05 20:20:10 -0600

    Added three states for the functionality of TodoList component

--- courseraPracticeIBM/todo_list_app/src/Components/TodoList.jsx
+++ courseraPracticeIBM/todo_list_app/src/Components/TodoList.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import './TodoList.css';
+import {useState} from 'react';
 
 const TodoList = () => {
-    
+    const [todos, setTodos] = useState([]);         // represents an array of the todo items 
+    const [headingInput, setHeadingInput] = useState('');          // represent the value entered by user into the input field for adding a new heading for a todo list 
+    const [listInputs, setListInputs] = useState({});       // initialize  listInputs as an empty object "{}". This state will hold the value  of input fields for each todo item individually
+
+    const handleAddTodo = () => {
+        if(headingInput.trim() !== ''){
+            setTodos([...todos, { heading : headingInput, lists : [] }]);
+            setHeadingInputs('');
+        }
+    }
+
+
     return(
         <>
             <div className="todo-container">
                 <h1 className="title">My Todo List</h1>
                 <div className="input-container">
-                    <input  type="text" className="heading-input" placeholder="Enter heading"/>
-                    <button className="add-list-button"></button>
+                    <input  type="text" className="heading-input" placeholder="Enter heading" 
+                    value={headingInput} onChange={(e) => {setHeadingInput(e.target.value); }}
+                    />
+                    <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
                 </div>
             </div>
+                
             <div className="todo-main">
 
             </div>
