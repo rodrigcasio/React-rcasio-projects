7bea1c1ac19207238ba56b89e6b84821d89201c0
rodrigcasio <rod.casio@outlook.com>
2025-09-05 18:53:41 -0600

    Added comments describing the purpose of this component

--- courseraPracticeIBM/practice_new_components_app/src/components/StyledComponentEx.jsx
+++ courseraPracticeIBM/practice_new_components_app/src/components/StyledComponentEx.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {useState} from 'react';
 
+// Example of a component with CSS style applied by using JavaScript objects 
+
 const StyledComponentEx = () => {
     const [isVisible, setIsVisible] = useState(true);
 
     const toggleVisibility = () => {
        setIsVisible(!isVisible);
     }
+    // object name:
     const messageStyle = {
-        display: isVisible ? 'block' : 'none',
+        display: isVisible ? 'block' : 'none',      // this is a conditional styling, dynamically sets the display property based on the value of isVisible.
         color: 'green',
         fontSize: '18px',
         marginTop: '10px',
