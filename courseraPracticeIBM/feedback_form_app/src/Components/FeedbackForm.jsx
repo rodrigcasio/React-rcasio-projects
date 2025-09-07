27fab9842fbab442710883469481736919c822ba
rodrigcasio <rod.casio@outlook.com>
2025-09-06 20:30:54 -0600

    Added content for the exercise in file "FeedbackForm.jsx & FeedbackForm.css"

--- courseraPracticeIBM/feedback_form_app/src/Components/FeedbackForm.jsx
+++ courseraPracticeIBM/feedback_form_app/src/Components/FeedbackForm.jsx
@@ -1,1 +1,19 @@
+import {useState} from 'react';
+import './FeedbackForm.css';
+
+const FeedbackForm = () => {
+    
+    return(
+        <>
+            <nav>Tell Us What You Think</nav>
+            <form className="feedback-form">
+                <h2>We'd Love To Hear From You!</h2>
+                <p>Please share your feedback with us.</p>
+            </form>
+        </>
+    );
+}
+export default FeedbackForm;
+
+
 
