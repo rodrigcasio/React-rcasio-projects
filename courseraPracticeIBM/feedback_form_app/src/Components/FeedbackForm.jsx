a77c674b7cb75434247679a88690712d94b668f3
rodrigcasio <rod.casio@outlook.com>
2025-09-07 17:58:27 -0600

    Fixed value of the input atribute value onChange to the name of the function "handleChange"

--- courseraPracticeIBM/feedback_form_app/src/Components/FeedbackForm.jsx
+++ courseraPracticeIBM/feedback_form_app/src/Components/FeedbackForm.jsx
@@ -16,8 +16,8 @@ const FeedbackForm = () => {
         const confirmationMessage = `   
             Name: ${formData.name}
             Email: ${formData.email}
-            Rating: ${formData.rating}
             Feedback: ${formData.feedback}
+            Rating: ${formData.rating}
             `;
         const isConfirmed = window.confirm(`Please confirm your details:\n${confirmationMessage}`); // displaying a confirmation dialogue presenting the current formData, if user confirms..user submits
         if(isConfirmed){
@@ -59,31 +59,31 @@ const FeedbackForm = () => {
                         type="radio"
                         name="rating"
                         value="1"
-                        onChange={formData.rating}
+                        onChange={handleChange}
                     /> 1</p>
                     <p><input 
                         type="radio"
                         name="rating"
                         value="2"
-                        onChange={formData.rating}
+                        onChange={handleChange}
                     /> 2</p>
                     <p><input 
                         type="radio"
                         name="rating"
                         value="3"
-                        onChange={formData.rating}
+                        onChange={handleChange}
                     /> 3</p>
                     <p><input 
                         type="radio"
                         name="rating"
                         value="4"
-                        onChange={formData.rating}
+                        onChange={handleChange}
                     /> 4</p>
                     <p><input 
                         type="radio"
                         name="rating"
                         value="5"
-                        onChange={formData.rating}
+                        onChange={handleChange}
                     /> 5</p>
                 </div>
                 <textarea
