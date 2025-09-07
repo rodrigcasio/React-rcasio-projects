837e126dc90bedd53c5cd84f7515bf7ac867efae
rodrigcasio <rod.casio@outlook.com>
2025-09-06 20:38:40 -0600

    Added basic template form

--- courseraPracticeIBM/feedback_form_app/src/Components/FeedbackForm.jsx
+++ courseraPracticeIBM/feedback_form_app/src/Components/FeedbackForm.jsx
@@ -1,4 +1,3 @@
-import {useState} from 'react';
 import './FeedbackForm.css';
 
 const FeedbackForm = () => {
@@ -7,6 +6,22 @@ const FeedbackForm = () => {
         <>
             <nav>Tell Us What You Think</nav>
             <form className="feedback-form">
+                <input 
+                    type="text"
+                    name="name"
+                    placeholder="Your Name"
+                />
+                <input 
+                    type="email"
+                    name="email"
+                    placeholder="Your Email"
+                />
+                <textarea
+                    name="feedback"
+                    placeholder="Your Feedback"
+                ></textarea>
+                <button type="submit">Submit Feedback</button>
+
                 <h2>We'd Love To Hear From You!</h2>
                 <p>Please share your feedback with us.</p>
             </form>
