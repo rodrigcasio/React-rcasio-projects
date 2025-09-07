d2b19f6ae828e8a9c37d35aec89eae8e506f218a
rodrigcasio <rod.casio@outlook.com>
2025-09-06 21:29:15 -0600

    Modified position of the <h2 & <p node element, and corrected style declaration for each input node element of the form

--- courseraPracticeIBM/feedback_form_app/src/Components/FeedbackForm.jsx
+++ courseraPracticeIBM/feedback_form_app/src/Components/FeedbackForm.jsx
@@ -34,7 +34,9 @@ const FeedbackForm = () => {
     return(
         <>
             <nav>Tell Us What You Think</nav>
-            <form className="feedback-form">
+            <form onSubmit={handleSubmit} className="feedback-form">
+                <h2>We'd Love To Hear From You!</h2>
+                <p>Please share your feedback with us.</p>
                 <input 
                     type="text"
                     name="name"
@@ -55,13 +57,7 @@ const FeedbackForm = () => {
                     value={formData.feedback}
                     onChange={handleChange}
                 ></textarea>
-                <button 
-                    className="feedback-form"
-                    type="submit" 
-                    onSumbit={handleSubmit}
-                >Submit Feedback</button>
-                <h2>We'd Love To Hear From You!</h2>
-                <p>Please share your feedback with us.</p>
+                <button type="submit" >Submit Feedback</button>
             </form>
         </>
     );
