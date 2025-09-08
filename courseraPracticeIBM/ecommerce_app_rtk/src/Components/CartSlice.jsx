121e7bd4bb21076be7fdc3a67be13acf526fdf84
rodrigcasio <rod.casio@outlook.com>
2025-09-08 17:18:07 -0600

    Imported Redux toolkit function "createSlice"

--- courseraPracticeIBM/ecommerce_app_rtk/src/Components/CartSlice.jsx
+++ courseraPracticeIBM/ecommerce_app_rtk/src/Components/CartSlice.jsx
@@ -1,6 +1,13 @@
 // Implementing logic for the Redux toolkit to ensure when "Add To Cart" is clicked, the info of the product qty should be available
 // globally to any component
+// for this file it is needed to install @reduxjs/toolkit and react-redux as a third-party module.
+import  { createSlice } from 'react-redux'
 
-const CartSlice =({
-    cartItems: [];      // initialize an empty array
+const CartSlice = createSlice({     // initialized with one createSlice Redux toolkit function
+
 });
+
+
+const initialState = {      // initialized an empty array
+    cartItems: [],
+};
