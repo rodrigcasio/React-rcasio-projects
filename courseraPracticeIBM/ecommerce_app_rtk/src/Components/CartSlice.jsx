930772a23b2bd3834217b7529fcf0860fb4efc1c
rodrigcasio <rod.casio@outlook.com>
2025-09-08 17:09:27 -0600

    Added description for the component "CartSlice.jsx"

--- courseraPracticeIBM/ecommerce_app_rtk/src/Components/CartSlice.jsx
+++ courseraPracticeIBM/ecommerce_app_rtk/src/Components/CartSlice.jsx
@@ -1,4 +1,6 @@
+// Implementing logic for the Redux toolkit to ensure when "Add To Cart" is clicked, the info of the product qty should be available
+// globally to any component
 
 const CartSlice =({
-
+    cartItems: [];      // initialize an empty array
 });
