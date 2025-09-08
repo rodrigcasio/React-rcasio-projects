4bc9e675a2e4ad6ce045167bd249ccbef6d3b05d
rodrigcasio <rod.casio@outlook.com>
2025-09-08 16:58:29 -0600

    Imported the components "ProductList.jsx and ShoppingCart.jsx" into the App.jsx parent component

--- courseraPracticeIBM/ecommerce_app_rtk/src/App.jsx
+++ courseraPracticeIBM/ecommerce_app_rtk/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import './App.css'
+import ProductList from './Components/ProductList';
+import ShoppingCart from './Components/ShoppingCart';
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <h1 className="app-heading">E-Commerce Application</h1>
+      <ProductList />
+      <ShoppingCart />
+    </div>
   )
 }
 
