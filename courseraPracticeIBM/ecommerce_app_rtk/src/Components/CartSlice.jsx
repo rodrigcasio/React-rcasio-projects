5114e6c331688448a913734700aa6836006e478f
rodrigcasio <rod.casio@outlook.com>
2025-09-08 19:21:46 -0600

    imported redux toolkit to file "CartSlice.jsx"

--- courseraPracticeIBM/ecommerce_app_rtk/src/Components/CartSlice.jsx
+++ courseraPracticeIBM/ecommerce_app_rtk/src/Components/CartSlice.jsx
@@ -1,7 +1,7 @@
 // Implementing logic for the Redux toolkit to ensure when "Add To Cart" is clicked, the info of the product qty should be available
 // globally to any component
 // for this file it is needed to install @reduxjs/toolkit and react-redux as a third-party module.
-import  { createSlice } from 'react-redux'
+import  { createSlice } from '@reduxjs/toolkit'
 
 
 // Slice Creation
