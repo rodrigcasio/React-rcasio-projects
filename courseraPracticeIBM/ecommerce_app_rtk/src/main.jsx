1aaf146aaae9f974c15ca778c8ddd0fedb4737e3
rodrigcasio <rod.casio@outlook.com>
2025-09-08 20:12:09 -0600

    Added comment to notify if defualt css is needed or not

--- courseraPracticeIBM/ecommerce_app_rtk/src/main.jsx
+++ courseraPracticeIBM/ecommerce_app_rtk/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './index.css';   // optional, delete if wanted to follow same style as the created .css files
 import App from './App.jsx';
 import { Provider } from 'react-redux';
 import store from './store.js';
