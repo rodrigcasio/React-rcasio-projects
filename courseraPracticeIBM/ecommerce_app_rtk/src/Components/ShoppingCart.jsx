839a4f35be2a4ead660b4cfb986f9ff733af9991
rodrigcasio <rod.casio@outlook.com>
2025-09-08 19:41:41 -0600

    Added a "$" sign to ln 48 of "ShoppingCart.jsx"

--- courseraPracticeIBM/ecommerce_app_rtk/src/Components/ShoppingCart.jsx
+++ courseraPracticeIBM/ecommerce_app_rtk/src/Components/ShoppingCart.jsx
@@ -45,7 +45,7 @@ const ShoppingCart = () => {
                 </ul>
                 <button className="clear-cart-btn" onClick={handleClearCart}>Clear Cart</button>
             </div>
-            <div>{totalAmount ? <div>The total amount is {totalAmount}</div> : ''}</div>
+            <div>{totalAmount ? <div>The total amount is ${totalAmount}</div> : ''}</div>
         </>
     );
 }
